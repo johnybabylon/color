95d02deb07d64f7148049e5fba922853f188dd04
johny babylon <ztredded@gmail.com>
2015-12-23 06:12:41 -0800

    Initial Commit

---
+++ Storyboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Storyboard
+//
+//  Created by elliott chavis on 12/23/15.
+//  Copyright (c) 2015 Robco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Storyboard/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Storyboard
+//
+//  Created by elliott chavis on 12/23/15.
+//  Copyright (c) 2015 Robco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
